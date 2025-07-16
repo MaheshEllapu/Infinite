b5002f42f7e95eef142ae5144982aa201c5f46f7
MAHESH ELLAPU <maheshel@infinite.com>
2025-07-16 12:16:10 +0530

    CodeChallenge submitted

---
+++ SQL/Coding Challenge/CodeChallenge1 (C# & SQL)/CodeChallenge1_CSharp/CodeChallenge1_CSharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenge1_CSharp
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DOJ { get; set; }
+        public string City { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>
+            {
+                new Employee { EmployeeID = 1001, FirstName = "Malcolm", LastName = "Daruwalla", Title = "Manager", DOB = DateTime.Parse("16-11-1984"), DOJ = DateTime.Parse("08-06-2011"), City = "Mumbai" },
+            new Employee { EmployeeID = 1002, FirstName = "Asdin", LastName = "Dhalla", Title = "AsstManager", DOB = DateTime.Parse("20-08-1994"), DOJ = DateTime.Parse("07-07-2012"), City = "Mumbai" },
+            new Employee { EmployeeID = 1003, FirstName = "Madhavi", LastName = "Oza", Title = "Consultant", DOB = DateTime.Parse("14-11-1987"), DOJ = DateTime.Parse("12-04-2015"), City = "Pune" },
+            new Employee { EmployeeID = 1004, FirstName = "Saba", LastName = "Shaikh", Title = "SE", DOB = DateTime.Parse("03-06-1990"), DOJ = DateTime.Parse("02-02-2016"), City = "Pune" },
+            new Employee { EmployeeID = 1005, FirstName = "Nazia", LastName = "Shaikh", Title = "SE", DOB = DateTime.Parse("08-03-1991"), DOJ = DateTime.Parse("02-02-2016"), City = "Mumbai" },
+            new Employee { EmployeeID = 1006, FirstName = "Amit", LastName = "Pathak", Title = "Consultant", DOB = DateTime.Parse("07-11-1989"), DOJ = DateTime.Parse("01-08-2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1007, FirstName = "Vijay", LastName = "Natrajan", Title = "Consultant", DOB = DateTime.Parse("02-12-1989"), DOJ = DateTime.Parse("01-06-2015"), City = "Mumbai" },
+            new Employee { EmployeeID = 1008, FirstName = "Rahul", LastName = "Dubey", Title = "Associate", DOB = DateTime.Parse("11-11-1993"), DOJ = DateTime.Parse("06-01-2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1009, FirstName = "Suresh", LastName = "Mistry", Title = "Associate", DOB = DateTime.Parse("12-08-1992"), DOJ = DateTime.Parse("03-12-2014"), City = "Chennai" },
+            new Employee { EmployeeID = 1010, FirstName = "Sumit", LastName = "Shah", Title = "Manager", DOB = DateTime.Parse("12-04-1991"), DOJ = DateTime.Parse("02-01-2016"), City = "Pune" }
+            };
+
+            Console.WriteLine("All Employees : ");
+            foreach(var emp in empList)
+            {
+                Console.WriteLine($"{emp.EmployeeID} {emp.FirstName} {emp.LastName} {emp.Title} {emp.DOB.ToShortDateString()} {emp.DOJ.ToShortDateString()} {emp.City}");
+            }
+
+            Console.WriteLine("Employees not in Mumbai : ");
+            var notInMumbai = empList.Where(e => e.City != "Mumbai");
+            foreach (var emp in notInMumbai)
+                PrintEmployee(emp);
+
+            Console.WriteLine("\nAsstManagers : ");
+            var asstManagers = empList.Where(e => e.Title == "AsstManager");
+            foreach (var emp in asstManagers)
+                PrintEmployee(emp);
+
+            Console.WriteLine("\nEmployees whose Last Name starts with 'S' : ");
+            var lastNameStartsWithS = empList.Where(e => e.LastName.StartsWith("S"));
+            foreach (var emp in lastNameStartsWithS)
+                PrintEmployee(emp);
+
+            Console.Read();
+        }
+        static void PrintEmployee(Employee emp)
+        {
+            Console.WriteLine($"{emp.EmployeeID}, {emp.FirstName} {emp.LastName}, {emp.Title}, DOB: {emp.DOB:dd-MM-yyyy}, DOJ: {emp.DOJ:dd-MM-yyyy}, City: {emp.City}");
+        }
+    }
+}
---
+++ SQL/Coding Challenge/CodeChallenge1 (C# & SQL)/CodeChallenge1_SQL.sql
@@ -0,0 +1,121 @@
+create database Assessments
+
+-------------------------------------- QUESTION 1 --------------------------------------
+create table Books
+(ID int primary key,
+Title varchar(50),
+Author varchar(50),
+ISBN varchar(30) unique,
+Published_Date datetime) 
+
+insert into books (ID,Title,Author,ISBN,Published_Date) values
+(1,'My First SQL Book','Mary Parker',981483029127,'2012-02-22 12:08:17'),
+(2,'My Second SQL Book','John Mayer',857300923713,'1972-07-03 09:22:45'),
+(3,'My Third SQL Book','Cary Flint',523120967812,'2015-10-18 14:05:44')
+
+--Write a query to fetch the details of the books written by author whose name ends with er.
+select * from books where author like '%er'
+
+create table reviews
+(ID int,
+Book_ID int,
+Reviewer_Name varchar(50),
+Content varchar(50),
+Rating int,
+Published_Date datetime)
+
+insert into reviews (ID,Book_ID,Reviewer_Name,Content,Rating,Published_Date) values
+(1,1,'John Smith','My First Review',4,'2017-12-05 05:50:11'),
+(2,2,'John Smith','My Second Review',5,'2017-10-13 15:05:12'),
+(3,2,'Alice Walker','Another Review',1,'2017-10-22 23:47:10')
+
+--Display the Title ,Author and ReviewerName for all the books from the above table.
+select b.Title, b.Author, r.Reviewer_Name from Books b join reviews r On b.id=r.Book_ID
+
+--Display the reviewer name who reviewed more than one book.
+select Reviewer_Name from reviews group by Reviewer_Name having count(distinct book_id)>1
+
+
+
+-------------------------------------- QUESTION 2 --------------------------------------
+
+create table Customers_table
+(ID int primary key,
+C_Name varchar(50),
+C_Age int,
+C_Address varchar(100),
+C_Salary decimal(10,2))
+
+select * from customers_table
+
+insert into Customers_table (ID,C_Name,C_Age,C_Address,C_Salary) values
+(1,'Ramesh',32,'Ahmedabad',2000.00),
+(2,'Khilan',25,'Delhi',1500.00),
+(3,'Kaushik',23,'Kota',2000.00),
+(4,'Chaitali',25,'Mumbai',6500.00),
+(5,'Hardik',27,'Bhopal',8500.00),
+(6,'Komal',22,'MP',4500.00),
+(7,'Muffy',24,'Indore',10000.00)
+
+--Display the Name for the customer from above customer table who live in same address which has character o anywhere in address
+select C_Name from Customers_table where C_Address like '%o%'
+
+create table Orders_table
+(OID int primary key,
+Order_Date datetime,
+Customer_ID int,
+Amount decimal(10,2),
+Foreign key(Customer_ID) references customers_table(ID))
+
+ insert into Orders_table(OID,Order_Date,Customer_ID,Amount) values
+ (102,'2009-10-08 00:00:00',3,3000),
+ (100,'2009-10-08 00:00:00',3,1500),
+ (101,'2009-11-20 00:00:00',2,1560),
+ (103,'2008-05-20 00:00:00',4,2060)
+
+ --Write a query to display the Date,Total no of customer placed order on same Date
+ select Order_Date, count(Customer_ID) as Total_Customers from Orders_table group by Order_Date
+
+-------------------------------------- QUESTION 3 --------------------------------------
+
+create table Employee_Table
+(ID int primary key,
+E_Name varchar(30),
+E_Age int,
+E_Address varchar(50),
+Salary decimal(10,2))
+
+insert into Employee_Table(ID,E_Name,E_Age,E_Address,Salary) values
+(1,'Ramesh',32,'Ahmedabad',2000.00),
+(2,'Khilan',25,'Delhi',1500.00),
+(3,'Kaushik',23,'Kota',2000.00),
+(4,'Chaitali',25,'Mumbai',6500.00),
+(5,'Hardik',27,'Bhopal',8500.00),
+(6,'Komal',22,'MP',null),
+(7,'Muffy',24,'Indore',null)
+
+--Display the Names of the Employee in lower case, whose salary is null
+select lower(E_Name) as EmployeeName from Employee_Table where Salary is null
+
+-------------------------------------- QUESTION 4 --------------------------------------
+
+create table StudentDetails
+(RegisterNo int primary key,
+S_Name varchar(30),
+S_Age int,
+S_Qualification varchar(20),
+S_MobileNo varchar(15),
+S_MailID varchar(50),
+S_Location varchar(50),
+S_Gender char(1))
+
+insert into StudentDetails(RegisterNo,S_Name,S_Age,S_Qualification,S_MobileNo,S_MailID,S_Location,S_Gender) values
+(2,'Sai',22,'B.E',9952836777,'Sai@gmail.com','Chennai','M'),
+(3,'Kumar',20,'BSC',7890125648,'Kumar@gmail.com','Madhurai','M'),
+(4,'Selvi',22,'B.Tech',8904567342,'Selvi@gmail.com','Selam','F'),
+(5,'Nisha',25,'M.E',7834672310,'Nisha@gmail.com','Theni','F'),
+(6,'SaiSaran',21,'B.A',7890345678,'Saran@gmail.com','Madhurai','F'),
+(7,'Tom',23,'BCA',8901234675,'Tom@gmail.com','Pune','M')
+
+--Write a sql server query to display the Gender,Total no of male and female from the above relation
+Select S_Gender,count(*) as Total from StudentDetails group by S_Gender
