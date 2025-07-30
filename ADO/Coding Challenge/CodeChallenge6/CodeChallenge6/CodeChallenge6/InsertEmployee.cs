ddd2c0aae16f1c9d9f609f34a914d3ec2e1ba112
MAHESH ELLAPU <maheshel@infinite.com>
2025-07-30 11:36:36 +0530

    CodeChallenge submitted

---
+++ ADO/Coding Challenge/CodeChallenge6/CodeChallenge6/CodeChallenge6/InsertEmployee.cs
@@ -0,0 +1,91 @@
+﻿//1.Write a stored Procedure that inserts records in the Employee_Details table
+ 
+//The procedure should generate the EmpId automatically to insert and should return the generated value to the user
+ 
+//Also the Salary Column is a calculated column (Salary is givenSalary - 10%)
+ 
+//Table: Employee_Details(Empid, Name, Salary, Gender)
+//Hint(User should not give the EmpId)
+
+
+//Test the Procedure using ADO classes and show the generated Empid and Salary
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeChallenge6
+{
+    class InsertEmployee
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+
+        static void Main(string[] args)
+        {
+            InsertData(); // calling insert using stored procedure
+            Console.ReadLine();
+        }
+
+        static SqlConnection getConnection()
+        {
+            con = new SqlConnection("Data Source=ICS-LT-2R6BJ84\\SQLEXPRESS; Initial Catalog=Assessments;user id=sa; password=MaheshEllapu@123;");
+            con.Open();
+            return con;
+        }
+
+        static void InsertData()
+        {
+            try
+            {
+                con = getConnection();
+
+                Console.WriteLine("Enter Employee Name:");
+                string empname = Console.ReadLine();
+
+                Console.WriteLine("Enter Gender (M/F):");
+                string gender = Console.ReadLine();
+
+                Console.WriteLine("Enter Given Salary:");
+                decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                cmd = new SqlCommand("InsertEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // input parameters (name must match stored procedure)
+                cmd.Parameters.AddWithValue("@Name", empname);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Salary", salary);
+
+                // output parameters
+                SqlParameter outEmpId = new SqlParameter("@GeneratedEmpid", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(outEmpId);
+
+                SqlParameter outNetSalary = new SqlParameter("@NetSalary", SqlDbType.Decimal)
+                {
+                    Precision = 10,
+                    Scale = 2,
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(outNetSalary);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("\nEmployee Record Inserted Successfully!");
+                Console.WriteLine("Generated EmpId     : " + outEmpId.Value);
+                Console.WriteLine("Calculated Net Salary: " + outNetSalary.Value);
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ADO/Coding Challenge/CodeChallenge6/CodeChallenge6/CodeChallenge6/UpdateSalary.cs
@@ -0,0 +1,78 @@
+﻿//2.Write a procedure that takes empid as input and outputs the updated salary as current salary + 100 for the give employee.
+ 
+//  Test the procedure using ADO classes and display the Employee details of that employee whose salary has been updated
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeChallenge6
+{
+    class UpdateSalary
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+
+        static void Main(string[] args)
+        {
+            UpdatingSalary();
+            Console.Read();
+        }
+
+        static SqlConnection getConnection()
+        {
+            con = new SqlConnection("Data Source=ICS-LT-2R6BJ84\\SQLEXPRESS; Initial Catalog=Assessments;user id=sa; password=MaheshEllapu@123;");
+            con.Open();
+            return con;
+        }
+
+        static void UpdatingSalary()
+        {
+            try
+            {
+                con = getConnection();
+
+                Console.Write("Enter Employee ID: ");
+                int empid = Convert.ToInt32(Console.ReadLine());
+
+                cmd = new SqlCommand("UpdateEmployeeSalary", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@empid", empid);
+
+                SqlParameter nameParam = new SqlParameter("@name", SqlDbType.VarChar, 100);
+                nameParam.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(nameParam);
+
+                SqlParameter genderParam = new SqlParameter("@gender", SqlDbType.Char, 1);
+                genderParam.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(genderParam);
+
+                SqlParameter updatedSalaryParam = new SqlParameter("@updated_salary", SqlDbType.Int);
+                updatedSalaryParam.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(updatedSalaryParam);
+
+                SqlParameter netSalaryParam = new SqlParameter("@net_salary", SqlDbType.Int);
+                netSalaryParam.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(netSalaryParam);
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("--- Updated Employee Details ---");
+                Console.WriteLine("EmpId          : " + empid);
+                Console.WriteLine("Name           : " + nameParam.Value);
+                Console.WriteLine("Gender         : " + genderParam.Value);
+                Console.WriteLine("Updated Salary : " + updatedSalaryParam.Value);
+                Console.WriteLine("Net Salary     : " + netSalaryParam.Value);
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
