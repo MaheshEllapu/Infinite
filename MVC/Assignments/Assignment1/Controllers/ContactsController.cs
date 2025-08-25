a564c93b843c597cf42aa9f036d4668fabeb3c18
MAHESH ELLAPU <maheshel@infinite.com>
2025-08-25 12:18:00 +0530

    Folder arranged

---
+++ MVC/Assignments/Assignment1/Controllers/ContactsController.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using assignment1.Models;
+using assignment1.Repository;
+
+namespace assignment1.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private ContactContext db = null;
+        private IContactRepository contactRepository = null;
+
+        public ContactsController()
+        {
+            db = new ContactContext();
+            contactRepository = new ContactRepository();
+        }
+
+        // GET: Contacts
+        public async Task<ActionResult> Index()
+        {
+            return View(await contactRepository.GetAllAsync());
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                await contactRepository.CreateAsync(contact);
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = await db.contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            await contactRepository.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
